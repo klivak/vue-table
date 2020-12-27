228eff03aba7bdb06857f67788413c7337a20cbb
Vlad <vladklivak@gmail.com>
2020-12-27 12:46:54 +0200

    init table page

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="table">
-    <h1>This is an table page</h1>
-  </div>
+  <section class="table">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>Table page</h1>
+        </div>
+        <div class="col-12">
+          table
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home'
+});
+</script>
+
+<style lang="scss">
+.table {
+  padding-top: 25px;
+
+  h1 {
+    margin-bottom: 25px;
+  }
+}
+</style>
